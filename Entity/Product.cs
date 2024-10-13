f37b719e74c93373e7dc92680896a06cfbe00af1
alaa <alaa@DESKTOP-10U3HBM>
2024-10-13 17:22:55 +0300

    Add project files.

---
+++ Entity/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Data.Entity
+{
+    public class Product:BaseEntity<int>
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+        public decimal Price { get; set; }
+
+        public ProductType ProductType { get; set; }
+        public int ProductTypeId { get; set; }
+
+
+
+        public ProductBrand ProductBrand { get; set; }
+        public int ProductBrandId { get; set; }
+
+       
+
+
+    }
+}
